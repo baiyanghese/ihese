827c7f24bce92c0c958fb3b2c1444f5cc800ef44
jackfan@isosoo.com <baiyang@cowork.cn>
2016-03-24 16:02:20 +0800

    添加联系我

--- wp-content/themes/freefire/index.php
+++ wp-content/themes/freefire/index.php
@@ -1,52 +1,86 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="format-detection"content="telephone=no, email=no" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="format-detection" content="telphone=no, email=no" />
-        <meta name="renderer" content="webkit" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="MobileOptimized" content="320" />
-        <meta name="screen-orientation" content="portrait" />
-        <meta name="x5-orientation" content="portrait"/>
-        <meta name="full-screen" content="yes"/>
-        <meta name="x5-fullscreen" content="true"/>
-        <meta name="browsermode" content="application"/>
-        <meta name="x5-page-mode" content="app"/>
-        <meta name="msapplication-tap-highlight" content="no"/>
-        <title>
-        荷色小站－－为享受左脑艺术的乐趣而生
-        </title>
-        <link href="/favicon.ico" rel="bookmark" type="image/x-icon" />
-        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
-        <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-        <meta name="keywords" content="" />
-        <meta name="description" content="" />
-        <meta name="author" content="JackFan,baiyanghese@gmail.com" />
-        <script type="text/javascript" src="/wp-content/themes/freefire/js/flexible.js"></script>
-        <link rel="stylesheet" href="/wp-content/themes/freefire/css/home.css" />
-    </head>
-    <body>
-      <canvas width="100%" height="100%" id="blogbg">
-        你的浏览器不支持哦～
-      </canvas>
-      <div class="blognav">
-        <a href="/" class="logo"></a>
-        <h2>为享受左脑艺术的乐趣而生</h2>
-        <div class="menu">
-          <a href="" class="active">文章</a>
-          <a href="">工具</a>
-          <a href="">导航</a>
-          <a href="">社区</a>
-        </div>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="format-detection"content="telephone=no, email=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="format-detection" content="telphone=no, email=no" />
+    <meta name="renderer" content="webkit" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="screen-orientation" content="portrait" />
+    <meta name="x5-orientation" content="portrait"/>
+    <meta name="full-screen" content="yes"/>
+    <meta name="x5-fullscreen" content="true"/>
+    <meta name="browsermode" content="application"/>
+    <meta name="x5-page-mode" content="app"/>
+    <meta name="msapplication-tap-highlight" content="no"/>
+    <title>
+    荷色小站&bull;为享受左脑艺术的乐趣而生
+    </title>
+    <link href="/favicon.ico" rel="bookmark" type="image/x-icon" />
+    <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="JackFan,baiyanghese@gmail.com" />
+    <script type="text/javascript" src="/wp-content/themes/freefire/js/flexible.js"></script>
+    <link rel="stylesheet" href="/wp-content/themes/freefire/css/home.css" />
+  </head>
+  <body>
+    <canvas width="100%" height="100%" id="blogbg">
+    你的浏览器不支持哦～
+    </canvas>
+    <div class="blognav">
+      <a href="/" class="logo"></a>
+      <h2>为享受左脑艺术的乐趣而生</h2>
+      <div class="menu">
+        <a href="" class="active">文章</a>
+        <a href="">工具</a>
+        <a href="">导航</a>
+        <a href="">社区</a>
       </div>
-      <div class="footer">
-        COPYRIGHT © 2016 IHESE.COM. ALL RIGHTS RESERVED.网站备案号：<a href="">粤ICP备13030603号</a><br />THEME DESIGN BY Jack Fan 荷色小站旗下网站，盗版必究
-      </div>
-      <script type="text/javascript" src="/wp-content/themes/freefire/js/zepto.min.js"></script>
-      <script type="text/javascript" src="/wp-content/themes/freefire/js/home.js"></script>
-    </body>
-  </html>
+    </div>
+    <div class="contactme">
+      <ul>
+        <li>
+          <a href="http://github.com/baiyanghese" class="atips" data-tips="GitHub">
+            <img src="/wp-content/themes/freefire/images/github.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;" class="atips" data-tips="暂未公布">
+            <img src="/wp-content/themes/freefire/images/weixin.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;" class="atips" data-tips="暂未公布">
+            <img src="/wp-content/themes/freefire/images/qq.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="http://weibo.com/fliyer" class="atips" data-tips="微博">
+            <img src="/wp-content/themes/freefire/images/weibo.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="http://twitter.com/imJackFan" class="atips" data-tips="twitter">
+            <img src="/wp-content/themes/freefire/images/twitter.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="http://facebook.com/imjackfan" class="atips" data-tips="facebook">
+            <img src="/wp-content/themes/freefire/images/facebook.png" alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer">
+      COPYRIGHT © 2016 IHESE.COM. ALL RIGHTS RESERVED.网站备案号：<a href="">粤ICP备13030603号</a><br />THEME DESIGN BY Jack Fan 荷色小站旗下网站，盗版必究
+    </div>
+    <script type="text/javascript" src="/wp-content/themes/freefire/js/jquery.js"></script>
+    <script type="text/javascript" src="/wp-content/themes/freefire/js/home.js"></script>
+  </body>
+</html>
