6e15090d1b4e74f78b57ab0b49b55f495d173073
jackfan@isosoo.com <baiyang@cowork.cn>
2016-03-29 17:52:16 +0800

    添加统计代码

--- wp-content/themes/freefire/footer.php
+++ wp-content/themes/freefire/footer.php
@@ -36,12 +36,19 @@
     </div>
   </section>
   <section class="footer">
-    <p>COPYRIGHT © 2016 IHESE.COM. ALL RIGHTS RESERVED.网站备案号：<a href="">粤ICP备13030603号</a></p><p>BASE ON WORDPRESS，THEME DESIGN BY Jack Fan 荷色小站旗下网站</p>
+    <p>COPYRIGHT © 2016 IHESE.COM. ALL RIGHTS RESERVED.网站备案号：<a href="http://www.miibeian.gov.cn/">粤ICP备13030603号</a></p><p>BASE ON WORDPRESS，THEME DESIGN BY Jack Fan 荷色小站旗下网站</p>
   </section>
 </footer>
 <script type="text/javascript" src="http://cdn.bootcss.com/modernizr/2.8.3/modernizr.js"></script>
 <script type="text/javascript" src="/wp-content/themes/freefire/js/jquery.js"></script>
 <script type="text/javascript" src="/wp-content/themes/freefire/js/highlight.js"></script>
 <script type="text/javascript" src="/wp-content/themes/freefire/js/ihese.js"></script>
+<div style="display:none">
+<script type="text/javascript" src="http://tajs.qq.com/stats?sId=18891412" charset="UTF-8"></script>
+<script type="text/javascript" src="http://js.tongji.linezing.com/3284132/tongji.js"></script>
+<noscript><a href="http://www.linezing.com"><img src="http://img.tongji.linezing.com/3284132/tongji.gif"/></a></noscript>
+<script language="javascript" type="text/javascript" src="http://js.users.51.la/17027829.js"></script>
+<noscript><a href="http://www.51.la/?17027829" target="_blank"><img alt="&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;" src="http://img.users.51.la/17027829.asp" style="border:none" /></a></noscript>
+</div>
 </body>
 </html>
