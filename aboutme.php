40532d3cb29c0a013ba46ca374a5206317fb304b
jackfan@isosoo.com <baiyang@cowork.cn>
2016-06-29 16:12:04 +0800

    添加关于我

---
+++ aboutme.php
@@ -0,0 +1,194 @@
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+        <link rel="pingback" href="http://b.ihese.com/xmlrpc.php">
+        <meta name="format-detection"content="telephone=no, email=no" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="renderer" content="webkit" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="MobileOptimized" content="320" />
+        <meta name="screen-orientation" content="portrait" />
+        <meta name="x5-orientation" content="portrait"/>
+        <meta name="full-screen" content="yes"/>
+        <meta name="x5-fullscreen" content="true"/>
+        <meta name="browsermode" content="application"/>
+        <meta name="x5-page-mode" content="app"/>
+        <meta name="msapplication-tap-highlight" content="no"/>
+        <title>
+            关于我 - 荷色小站(iHese.com)&bull;为享受左脑艺术的乐趣而生
+        </title>
+        <link href="/favicon.ico" rel="bookmark" type="image/x-icon" />
+        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+        <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <meta name="keywords" content="荷色小站、iHese.com、WEB开发笔记、前端开发、网站开发、软件设计、网页设计、移动端研发、APP设计与制作" />
+        <meta name="description" content="荷色小站(iHese.com)&bull;为享受左脑艺术的乐趣而生--专注网站开发笔记，Web技术的学习与研究，关注设计、互联网，一个记录、分享互联网知识与生活的网站" />
+        <meta name="author" content="JackFan,baiyanghese@gmail.com" />
+        <script type="text/javascript" src="/wp-content/themes/freefire/js/flexible.js"></script>
+        <link rel="stylesheet" href="/wp-content/themes/freefire/css/hljs.css">
+        <link rel="stylesheet" href="/wp-content/themes/freefire/css/animate.css">
+        <link rel="stylesheet" href="/wp-content/themes/freefire/css/ihese.css"
+        />
+    </head>
+    <body>
+    <div class="status" id="loading">
+      <div class="l-loading-panel">
+        <span class="l-loading"></span>
+      </div>
+    </div>
+    <heaher class="header">
+        <section>
+            <h1 class="website-name">
+            关于我
+            </h1>
+        </section>
+        <section>
+            <div class="website-nav">
+                <ul>
+                    <li>
+                        <a href="http://www.ihese.com" class="atips" data-tips="首页">
+                            <img src="/wp-content/themes/freefire/images/home.png" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" class="atips" data-tips="文">
+                            <img src="/wp-content/themes/freefire/images/artice.png" alt="" />
+                        </a>
+                    </li>
+                   <!--<li>
+                        <a href="/" class="atips" data-tips="留言">
+                            <img src="/wp-content/themes/freefire/images/feedbook.png" alt="" />
+                        </a>
+                    </li>-->
+                    <li>
+                        <a href="/" class="atips" data-tips="域名列表">
+                            <img src="/wp-content/themes/freefire/images/reward.png" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/aboutme.php" class="atips" data-tips="关于">
+                            <img src="/wp-content/themes/freefire/images/about.png" alt="" />
+                        </a>
+                    </li>
+                    <!-- <li>
+                        <a href="/" class="atips" data-tips="兄弟站">
+                            <img src="/wp-content/themes/freefire/images/www.png" alt="" />
+                        </a>
+                    </li> -->
+                </ul>
+            </div>
+        </section>
+    </heaher>
+
+<main class="min-container single">
+    <section>
+        <div class="aboutme-avatar">
+            <img src="/wp-content/themes/freefire/images/jackfan.jpg" alt="JackFan" />
+        </div>
+        <h2 class="aboutme-title">Jack Fan</h2>
+    </section>
+    <section class="aboutme-content">
+        <p>
+我是JackFan，毕业于09年，从事着WEB相关的研发，酷爱前端领域各种开发（PC/无线），捣鼓过各种UI设计（WEB/APP），折腾过Linux，做过几个小项目，喜欢收藏域名，不爱运动，爱电影。<br /><br />
+
+关注生活，信仰互联网，渴望能改变世界。<br /><br />
+
+闲暇时光喜欢折腾小程序，便有了荷色小站，小站目前开放三大板块（<a href="http://b.ihese.com">文章</a>、<a href="http://fanli.ihese.com">返利</a>、<a href="http://wx.ihese.com">精选</a>）。<br /><br />
+
+<a href="http://b.ihese.com">文章</a>，只为能坚持记录生活与工作的点滴。<br /><br />
+
+正如王小波所说：假如我要写什么，我根本就不管他格调不格调，正如谈恋爱时我绝不从爱祖国开始谈起。<br /><br />
+
+<a href="http://fanli.ihese.com">返利</a>，只为帮助一些人购物省钱，节省是一种美德，杜绝浪费。<br /><br />
+
+<a href="http://wx.ihese.com">精选</a>，只为帮助阅读微信公众号文章时提升体验，无需关注过多公众号便可阅读好文章，奇文共欣赏，疑义相如析。<br /><br />
+
+关于网名，曾用马甲无数，白洋荷色居首，白洋次之。也正因如此才有了域名（IHESE.COM）。社会在进步，咱不能拖后腿，为了与国际接轨，为了适应今天的环境。从今往后，便有了洋名：JackFan。<br /><br />
+
+我对自己的要求很低：我活在世上，无非想要明白些道理，遇见些有趣的事。倘能如我愿，我的一生就算成功（其实这句仍是王小波所说）。<br /><br />
+
+荷色小站，为享受左脑艺术的乐趣而生！<br /><br />
+
+以上我所说的全都是错的。
+        </p>
+    </section>
+        <!--高速版-->
+<div id="SOHUCS"></div>
+<script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
+<script type="text/javascript">
+    window.changyan.api.config({
+        appid: 'cyst8CjAG',
+        conf: 'prod_9322ddac6f9045543393d4bc3dab82b1'
+    });
+</script>
+</main>
+<footer>
+  <section>
+    <div class="contact-me">
+      <ul>
+        <li>
+          <a href="http://github.com/baiyanghese" class="atips" data-tips="GitHub">
+            <img src="/wp-content/themes/freefire/images/github.png" alt="" />
+          </a>
+        </li>
+        <!-- <li>
+          <a href="javascript:;" class="atips" data-tips="暂未公布">
+            <img src="/wp-content/themes/freefire/images/weixin.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;" class="atips" data-tips="暂未公布">
+            <img src="/wp-content/themes/freefire/images/qq.png" alt="" />
+          </a>
+        </li> -->
+        <li>
+          <a href="http://weibo.com/fliyer" class="atips" data-tips="微博">
+            <img src="/wp-content/themes/freefire/images/weibo.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="http://twitter.com/imJackFan" class="atips" data-tips="twitter">
+            <img src="/wp-content/themes/freefire/images/twitter.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="http://facebook.com/imjackfan" class="atips" data-tips="facebook">
+            <img src="/wp-content/themes/freefire/images/facebook.png" alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <section class="footer">
+    <p>COPYRIGHT © 2016 IHESE.COM. ALL RIGHTS RESERVED.网站备案号：<a href="http://www.miibeian.gov.cn/">粤ICP备13030603号</a></p><p>BASE ON WORDPRESS，THEME DESIGN BY Jack Fan 荷色小站旗下网站</p>
+  </section>
+</footer>
+<script type="text/javascript" src="http://cdn.bootcss.com/modernizr/2.8.3/modernizr.js"></script>
+<script type="text/javascript" src="/wp-content/themes/freefire/js/jquery.js"></script>
+<script type="text/javascript" src="/wp-content/themes/freefire/js/highlight.js"></script>
+<script type="text/javascript" src="/wp-content/themes/freefire/js/ihese.js"></script>
+<style type="text/css">
+    #SOHUCS #SOHU_MAIN .module-hot-topic,#SOHUCS #SOHU_MAIN .module-cmt-footer .section-service-w, #SOHUCS #SOHU_MAIN .module-cmt-footer .section-service-w .service-wrap-w, #SOHUCS #SOHU_MAIN .module-cmt-footer .section-service-w .service-wrap-w a,#cyReward #cy-reward-pop .power-cy {
+        display: none!important;
+    }
+</style>
+<div style="display:none">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?17c455e0c2990c952d7528f05a90a898";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<script type="text/javascript" src="http://tajs.qq.com/stats?sId=18891412" charset="UTF-8"></script>
+<script type="text/javascript" src="http://js.tongji.linezing.com/3284132/tongji.js"></script>
+<noscript><a href="http://www.linezing.com"><img src="http://img.tongji.linezing.com/3284132/tongji.gif"/></a></noscript>
+</div>
+</body>
+</html>
