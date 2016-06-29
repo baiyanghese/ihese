f4aa1afbd6ba6bfc3a0de26e4f75b91807a5d7ea
jackfan@isosoo.com <baiyang@cowork.cn>
2016-06-29 16:40:52 +0800

    添加手机版兼容的留言

--- aboutme.php
+++ aboutme.php
@@ -118,70 +118,6 @@
     </section>
         <!--高速版-->
 <div id="SOHUCS"></div>
-<?php
-    function isMobile() {
-        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
-        if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
-            return true;
-        }
-        //如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
-        if (isset($_SERVER['HTTP_VIA'])) {
-            //找不到为flase,否则为true
-            return stristr($_SERVER['HTTP_VIA'], "wap")?true:false;
-        }
-        //脑残法，判断手机发送的客户端标志,兼容性有待提高
-        if (isset($_SERVER['HTTP_USER_AGENT'])) {
-            $clientkeywords = array(
-                'nokia',
-                'sony',
-                'ericsson',
-                'mot',
-                'samsung',
-                'htc',
-                'sgh',
-                'lg',
-                'sharp',
-                'sie-',
-                'philips',
-                'panasonic',
-                'alcatel',
-                'lenovo',
-                'iphone',
-                'ipod',
-                'blackberry',
-                'meizu',
-                'android',
-                'netfront',
-                'symbian',
-                'ucweb',
-                'windowsce',
-                'palm',
-                'operamini',
-                'operamobi',
-                'openwave',
-                'nexusone',
-                'cldc',
-                'midp',
-                'wap',
-                'mobile',
-            );
-            // 从HTTP_USER_AGENT中查找手机浏览器的关键字
-            if (preg_match("/(".implode('|', $clientkeywords).")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
-                return true;
-            }
-        }
-        //协议法，因为有可能不准确，放到最后判断
-        if (isset($_SERVER['HTTP_ACCEPT'])) {
-            // 如果只支持wml并且不支持html那一定是移动设备
-            // 如果支持wml和html但是wml在html之前则是移动设备
-            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
-                return true;
-            }
-        }
-        return false;
-    }
-    if (!isMobile()) {
-?>
 <script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
 <script type="text/javascript">
     window.changyan.api.config({
@@ -189,11 +125,6 @@
         conf: 'prod_9322ddac6f9045543393d4bc3dab82b1'
     });
 </script>
-<?php } else {?>
-<script id="changyan_mobile_js" charset="utf-8" type="text/javascript"
-    src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=cyst8CjAG&conf=prod_9322ddac6f9045543393d4bc3dab82b1">
-</script>
-<?php }?>
 </main>
 <footer>
   <section>
