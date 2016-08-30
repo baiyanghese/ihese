2a4ea0ff56380a1a854781b82b9a7fc8930cccd7
jackfan@isosoo.com <baiyang@cowork.cn>
2016-08-30 16:46:58 +0800

    添加google广告

--- about.php
+++ about.php
@@ -190,8 +190,6 @@ var _hmt = _hmt || [];
 })();
 </script>
 <script type="text/javascript" src="http://tajs.qq.com/stats?sId=18891412" charset="UTF-8"></script>
-<script type="text/javascript" src="http://js.tongji.linezing.com/3284132/tongji.js"></script>
-<noscript><a href="http://www.linezing.com"><img src="http://img.tongji.linezing.com/3284132/tongji.gif"/></a></noscript>
 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1259979453'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1259979453' type='text/javascript'%3E%3C/script%3E"));</script>
 </div>
 </body>
